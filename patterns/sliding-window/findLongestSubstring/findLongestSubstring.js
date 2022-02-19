b38422ee3987de998d2cc23a2fa7e48b0c8dac0f
Madhu Kiran Varma <28980183+mmkvdev@users.noreply.github.com>
2022-02-19 09:49:10 +0000

    pattern/sliding-window: findLongestSubstring

--- patterns/sliding-window/findLongestSubstring/findLongestSubstring.js
+++ patterns/sliding-window/findLongestSubstring/findLongestSubstring.js
@@ -6,7 +6,6 @@ const findLongestSubstring = (str) => {
   if (!str) {
     return null;
   }
-
   let longest = 0;
   let start = 0;
   let seen = {};
@@ -19,7 +18,6 @@ const findLongestSubstring = (str) => {
     longest = Math.max(longest, i - start + 1);
     seen[char] = i + 1;
   }
-
   return longest;
 }
 
