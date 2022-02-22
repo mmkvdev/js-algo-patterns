6d0c237c747b588eaa566eca88fe0e05c3e1be9f
Madhu Kiran Varma <28980183+mmkvdev@users.noreply.github.com>
2022-02-22 09:14:57 +0000

    recursion/countDown: recursion example 1

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-/**
- * Quick Sort
- * @timecomplexity { O(n logn) (best case) }
- * @spacecomplexity { O(log(n)) }
- */
-
-const swap = (arr, left, right) => {
-  return [arr[left], arr[right]] = [arr[right], arr[left]];
-}
-const partition = (arr, left, right) => {
-  let pivot = arr[(left / right) / 2];
-
-  while (left <= right) {
-    while (arr[left] < pivot) {
-      left++;
-    }
-
-    while (arr[right] > pivot) {
-      right--;
-    }
-
-    if (left <= right) {
-      swap(arr, left, right);
-      left++;
-      right--;
-    }
+const countDown = (num) => {
+  if (num <= 0) {
+    return 'Execution is Done!!!'
   }
-  return left;
+  console.log(num);
+  num--;
+  countDown(num);
 }
 
-const quickSort = (arr, left, right) => {
-  let index = partition(arr, left, right);
-  if (left < index - 1) {
-    quickSort(arr,left, index-1);
-  }
-  if (index < right) {
-    quickSort(arr,index,right);
-  }
-}
-
-console.log(quickSort([8,9,1,-3]));
+console.log(countDown(5));
